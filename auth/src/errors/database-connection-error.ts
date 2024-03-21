4f8e6cf4b64848494720c7e307aebb62ba36cc68
PatF3lix <pat_pg@hotmail.fr>
2024-03-21 16:13:07 +0000

    mongo depl pods configured, installed mongoose library. Created the User schema. Added Custom Error Class for polymorphism, every error class now extends Custom error abstract class

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,4 +1,4 @@
-import { CustomError } from "./customError";
+import { CustomError } from "./custom-error";
 export class DatabaseConnectionError extends CustomError {
   reason = "Error connecting to database";
   statusCode = 500;
