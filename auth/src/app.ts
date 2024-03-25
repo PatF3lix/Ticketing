0caf61656fc84f05e83f591accf50e58dfdedcd3
PatF3lix <pat_pg@hotmail.fr>
2024-03-25 11:53:49 +0000

    setting up auth service for testing using jest and supertest. Refactored index.ts and create app.ts isolating the express router configurations

---
+++ auth/src/app.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handlers";
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+/*tells express that it's behind a proxy of ingress engine x,
+ and to make sure that it should still trust traffic as being secure,
+ event though it's coming from that proxy.*/
+app.set("trust proxy", true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
