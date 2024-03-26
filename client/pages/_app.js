3887ed1d5d937e8c1608252e347d8db7c4dcbf23
PatF3lix <pat_pg@hotmail.fr>
2024-03-26 16:34:31 +0000

    signin file/route created

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
 
 const app = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <header>Header!</header>
+      <Component {...pageProps} />;
+    </div>
+  );
 };
 
 export default app;
