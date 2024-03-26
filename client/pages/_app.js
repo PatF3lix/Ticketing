abf334da9783c88be7b6a41b9ca464ef9c7e8799
PatF3lix <pat_pg@hotmail.fr>
2024-03-26 17:07:49 +0000

    implemented reactive header component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,30 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Header from "../components/header";
 
-const app = ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
-      <header>Header!</header>
+      <Header currentUser={currentUser} />
       <Component {...pageProps} />;
     </div>
   );
 };
 
-export default app;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  // get information for the landing page getInitialProps function.
+  // This will be passd down through props
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext);
+  }
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
