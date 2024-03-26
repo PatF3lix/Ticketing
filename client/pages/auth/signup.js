6aea9b48f5a6be15e8f1128335f2ddee51a19f09
PatF3lix <pat_pg@hotmail.fr>
2024-03-26 13:19:42 +0000

    signup page implemented. Created custom hook useRequest in order to make dynamic request and error handling to the auth service. Displaying the errors to the u.i and redirecting the user to the landing page if the user signup properly

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,15 +1,46 @@
+import { useState } from "react";
+import useRequest from "../../hooks/use-request";
+import Router from "next/router";
+
 const signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push("/"),
+  });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    doRequest();
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign up</h1>
       <div className="form-group">
         <label>Email Address</label>
-        <input className="form-control" />
+        <input
+          value={email}
+          onChange={(e) => setEmail((email) => e.target.value)}
+          className="form-control"
+        />
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input type="password" className="form-control" />
+        <input
+          value={password}
+          onChange={(e) => setPassword((password) => e.target.value)}
+          type="password"
+          className="form-control"
+        />
       </div>
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
