c2f47184aeaa9484e66729a0ebd8bfc3747c4046
PatF3lix <pat_pg@hotmail.fr>
2024-04-03 20:40:31 +0100

    nats-wrapper mock file refactoring

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/tickets";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("has a route handler listening yo /api/tickets for post requests", async () => {
   const res = await request(app).post("/api/tickets").send({});
@@ -73,4 +74,18 @@ it("creates a ticket with valid inputs", async () => {
 
   tickets = await Ticket.find({});
   expect(tickets.length).toEqual(1);
+});
+
+it("publishes an event", async () => {
+  const title = "asdfgh";
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title,
+      price: 20,
+    })
+    .expect(201);
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
