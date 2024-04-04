f3283ef760c86e3e5512d7f7ff73f5ef03ec5c87
PatF3lix <pat_pg@hotmail.fr>
2024-04-04 21:47:19 +0100

    implemented test and route logic for the orders show, new, and get by index requests

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from "express";
+import { NotAuthorizedError, requireAuth } from "@microserviceticket/common";
+import { Order, OrderStatus } from "../models/order";
+import { NotFoundError } from "@microserviceticket/common";
 
 const router = express.Router();
 
 router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
+  const order = await Order.findById(req.params.orderId).populate("ticket");
+  if (!order) {
+    throw new NotFoundError();
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+  res.send(order);
 });
 
 export { router as showOrderRouter };
