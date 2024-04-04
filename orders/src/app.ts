bd7637853e8c5080b03189b66f8ce330a1080b06
PatF3lix <pat_pg@hotmail.fr>
2024-04-04 11:18:34 +0100

    initial orders service setup completed. skaffold update, ingress-srv orders path config updated. oders depl created and orders mongo depl created. required packages installed and skaffold restarted.

---
+++ orders/src/app.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@microserviceticket/common";
+
+import { createticketsRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
+
+const app = express();
+/*tells express that it's behind a proxy of ingress engine x,
+ and to make sure that it should still trust traffic as being secure,
+ event though it's coming from that proxy.*/
+app.set("trust proxy", true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.use(currentUser);
+app.use(createticketsRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
