c9284a7addd05a6b0e2aae6b2e34fc32fc6442fc
PatF3lix <pat_pg@hotmail.fr>
2024-04-04 13:33:42 +0100

    new order creation code implementation almost complete. Starting testing

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import { Order, OrderStatus } from "./order";
 interface TicketAttrs {
   title: string;
   price: number;
@@ -9,6 +9,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  isReserved(): Promise<Boolean>;
   //   userId: string;
 }
 
@@ -45,6 +46,28 @@ const ticketSchema = new mongoose.Schema(
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
+};
+
+//this is how we add a method directly to the ticekt document itself
+ticketSchema.methods.isReserved = async function () {
+  /** Run query to look at all orders.
+     Find an order where the ticket is the ticket we just found
+     *and* the orders status is *not* cancelled.
+     If we find an order from that means the ticket *is* reserved */
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
+  // in case existing order is null, it will be flipped to true, then false due to the (!!)
+  // in case the existing order exists, it will be flipped to false, then true due to the (!!)
+  return !!existingOrder;
 };
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
