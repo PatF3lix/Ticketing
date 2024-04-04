5bec34625269a278bab81ac38419b89ce19e0326
PatF3lix <pat_pg@hotmail.fr>
2024-04-04 12:08:40 +0100

    implemented the basic routes required to make the orders service deploy without any erros. orders model folder and file created

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -8,10 +8,10 @@ import {
   currentUser,
 } from "@microserviceticket/common";
 
-import { createticketsRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes/index";
+import { deleteOrderRouter } from "./routes/delete";
 
 const app = express();
 /*tells express that it's behind a proxy of ingress engine x,
@@ -28,10 +28,10 @@ app.use(
 );
 
 app.use(currentUser);
-app.use(createticketsRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
