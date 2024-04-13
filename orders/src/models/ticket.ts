0f6db32105debb9551159259f93925a234b64817
PatF3lix <pat_pg@hotmail.fr>
2024-04-13 12:04:12 +0100

    updated microservice/common package in orders and tickets add the ticket-updated listener. Added the event emitted version number to the OrderDoc and ticketDoc interface to keep track of emitted event order

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,21 +1,25 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 import { Order, OrderStatus } from "./order";
 interface TicketAttrs {
   id: string;
   title: string;
   price: number;
-  //   userId: string;
 }
 
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  version: number;
   isReserved(): Promise<Boolean>;
-  //   userId: string;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -29,10 +33,6 @@ const ticketSchema = new mongoose.Schema(
       required: true,
       min: 0,
     },
-    //   userId: {
-    //     type: String,
-    //     required: true,
-    //   },
   },
   {
     toJSON: {
@@ -45,14 +45,28 @@ const ticketSchema = new mongoose.Schema(
   }
 );
 
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+  return Ticket.findOne({ _id: event.id, version: event.version - 1 });
+};
+
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
     _id: attrs.id,
     title: attrs.title,
     price: attrs.price,
-    //   userId: attrs.userId,
   });
 };
+
+ticketSchema.set("versionKey", "version");
+ticketSchema.plugin(updateIfCurrentPlugin);
+
+// ticketSchema.pre("save", function (done) {
+//   // @ts-ignore
+//   this.$where = {
+//     version: this.get("version") - 1,
+//   };
+//   done();
+// });
 
 //this is how we add a method directly to the ticekt document itself
 ticketSchema.methods.isReserved = async function () {
