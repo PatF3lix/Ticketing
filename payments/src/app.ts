d19fc8de57c1bcb0dd28a74983b8f74f304611fd
PatF3lix <pat_pg@hotmail.fr>
2024-04-15 11:47:08 +0100

    payments service initial configuration completed

---
+++ payments/src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@microserviceticket/common";
+
+const app = express();
+/*tells express that it's behind a proxy of ingress engine x,
+ and to make sure that it should still trust traffic as being secure,
+ event though it's coming from that proxy.*/
+app.set("trust proxy", true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.use(currentUser);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
