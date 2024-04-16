57a49ce0738a99bc6cf76423c2eb007e56c4be5b
PatF3lix <pat_pg@hotmail.fr>
2024-04-16 15:02:31 +0100

    stripe package install and stripe payments functionality added to new.ts /api/payments route with appropriate tests. Stripe_key env variable added

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
   OrderStatus,
 } from "@microserviceticket/common";
 import { Order } from "../models/order";
+import { stripe } from "../stripe";
 
 const router = express.Router();
 
@@ -33,7 +34,13 @@ router.post(
       throw new BadRequestError("Cannot pay for a cancelled order");
     }
 
-    res.send({ success: true });
+    await stripe.charges.create({
+      currency: "usd",
+      amount: order.price * 100,
+      source: token,
+    });
+
+    res.status(201).send({ success: true });
   }
 );
 
