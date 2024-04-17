571d1d96d7fd9691aecb3a2889cf1c29aa5a9c7b
PatF3lix <pat_pg@hotmail.fr>
2024-04-17 11:53:45 +0100

    orders/orderId route create.Once purchase button is clicked you are redirected to the orders/orderId page to confirm and start payment

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,5 +1,33 @@
-const TicketShow = () => {
-  return <div>TicketShow</div>;
+import useRequest from "../../hooks/use-request";
+import Router from "next/router";
+
+const TicketShow = ({ ticket }) => {
+  const { doRequest, errors } = useRequest({
+    url: "/api/orders",
+    method: "post",
+    body: {
+      ticketId: ticket.id,
+    },
+    onSuccess: (order) =>
+      Router.push("/orders/[orderId]", `/orders/${order.id}`),
+  });
+
+  return (
+    <div>
+      <h1>Title: {ticket.title}</h1>
+      <h1>Price: {ticket.price}</h1>
+      {errors}
+      <button onClick={doRequest} className="btn btn-primary mt-3">
+        Purchase
+      </button>
+    </div>
+  );
+};
+
+TicketShow.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+  return { ticket: data };
 };
 
 export default TicketShow;
